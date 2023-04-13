7fce110ac7d86e55517a0098e806b9492aad1403
Akash <akash@gmail.com>
2023-04-13 16:39:17 +0530

    project done

---
+++ js/playlist.js
@@ -0,0 +1,45 @@
+let songs = [
+    {
+        name: "Apna Bana Le",
+        artist: "Arjit Singh",
+        img: "img1",
+        audio: "music1"
+    },
+    {
+        name: "Jungle Mein Kaand",
+        artist: "Vishal Dadlani",
+        img: "img2",
+        audio: "music2"
+    },
+    {
+        name: "Tu Maan Meri Jaan",
+        artist: "King",
+        img: "img3",
+        audio: "music3"
+    },
+    {
+        name: "Main Khiladi",
+        artist: "Abhijit",
+        img: "img4",
+        audio: "music4"
+    },
+    {
+        name: "Pyaar Hota Kayi Baar",
+        artist: "Arjit Singh",
+        img: "img5",
+        audio: "music5"
+    },
+    {
+        name: "Yeh Ek Zindagi",
+        artist: "Achint",
+        img: "img6",
+        audio: "music6"
+    },
+    {
+        name: "Tere Pyaar Mein",
+        artist: "Arjit Singh",
+        img: "img7",
+        audio: "music7"
+    }
+]
+// Achint , Yeh Ek Zindagi
